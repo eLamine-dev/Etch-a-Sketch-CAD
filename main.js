a0c89ec2f52ad8a135b0efb725c18d700fd3251f
e-lamine <lamine@e-lamine.dev>
2022-10-25 20:20:56 +0100

    found basic logic for coloring brush

--- main.js
+++ main.js
@@ -6,54 +6,145 @@ let canvas = document.getElementById('canvas');
 
 for (let i = 0; i < 2304; i++) {
 
-    let square = document.createElement('div');
-    square.classList.add('square');
+    let gridItem = document.createElement('div');
+    gridItem.classList.add('gridItem');
 
     let box = document.createElement('div');
     box.classList.add('box');
-    square.appendChild(box);
+    gridItem.appendChild(box);
 
     
     let gridLineV = document.createElement('div');
     gridLineV.classList.add('gridLineV');
-    square.appendChild(gridLineV);
-    gridLineV.addEventListener('click', ()=>{ square.style.gridTemplateColumns = 'auto 20%'; 
-                                              gridLineV.style.backgroundColor = 'red'})
+    gridItem.appendChild(gridLineV);
+   
 
     let gridLineH = document.createElement('div');
     gridLineH.classList.add('gridLineH');
-    square.appendChild(gridLineH);
+    gridItem.appendChild(gridLineH);
     
 
     let point = document.createElement('div');
     point.classList.add('point');
-    square.appendChild(point);
+    gridItem.appendChild(point);
 
-    container.appendChild(square);
+    container.appendChild(gridItem);
 
 
     }
 
-
-    // square.addEventListener('click', ()=>{
-
-    //     square.style.gridTemplateColumns = 'auto 20%';
+    let leftTools = document.getElementById('leftTools');
+    let rightTools = document.getElementById('rightTools');
 
 
+
+for (let i = 1; i <= 15; i++) {
+ 
+        let button = document.createElement('button');
+        button.id = `leftBtn${i}` ;
+        button.classList.add('tooltip');
+        let buttonImg = document.createElement('img')
+        buttonImg.classList.add('buttonImg');
+        button.appendChild(buttonImg);
+        let buttonTip = document.createElement('span');
+        buttonTip.classList.add('tooltiptext');
+        button.appendChild(buttonTip);
+
+        leftTools.appendChild(button);   
+    }
+
+for (let i = 1; i <= 15; i++) {   
+        let button = document.createElement('button');
+        button.id = `rightBtn${i}` ;
+        button.classList.add('tooltip');
+        let buttonImg = document.createElement('img')
+        buttonImg.classList.add('buttonImg');
+        button.appendChild(buttonImg);
+        let buttonTip = document.createElement('span');
+        buttonTip.classList.add('tooltiptext');
+        button.appendChild(buttonTip);
+
+        rightTools.appendChild(button); 
+    }
+
+function changeBoxColor(e) {
+    e.preventDefault();
+    this.style.backgroundColor = "red";
+}
+
+function startPainting(e){
+    e.preventDefault();
+    const boxes = document.querySelectorAll('.box');
+    boxes.forEach(box => box.addEventListener('mousedown', changeBoxColor)); // to paint the first box pressed
+    boxes.forEach(box => box.addEventListener('mouseenter', changeBoxColor));
+}
+
+function stopPainting(e){
+    e.preventDefault();
+    const boxes = document.querySelectorAll('.box');
+    boxes.forEach(box => box.removeEventListener('mouseenter', changeBoxColor));
+}
+
+
+
+document.getElementById('leftBtn1').addEventListener('click', ()=> {
+    container.addEventListener('mousedown', startPainting);
+    container.addEventListener('mouseup', stopPainting);
+})
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //     let squares = document.querySelectorAll('.gridItem');
+
+    //     function changeColor(target)  { target.firstChild.style.backgroundColor = "red"; };
+
+    //     squares.forEach(gridItem => { 
+            
+    //     gridItem.addEventListener('mousedown', (evt)=> {
+    //             evt.preventDefault();
+    //             squares.forEach(gridItem => {
+    //                 gridItem.addEventListener('mouseover',(evt) => {
+                       
+    //                     changeColor(gridItem)
+
+    //                 })
+                   
+    //             })
+       
+    //         })
+    //     });
+    // })
+    
+
+    // container.addEventListener('mouseup', (evt)=> {
+    //     evt.preventDefault();
+    //     squares.forEach(gridItem => {
+    //         gridItem.removeEventListener('mouseover', () => changeColor(gridItem))
+    // });
+  
     // }) 
-
-    
-
-
-
-
-    
-
-
-
-
-
-
-
-
-
